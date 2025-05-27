4b3f8a067c7e369f5a1cac271023f3edc065d1ae
LouWiebenga <LI.Wiebenga@student.han.nl>
2025-05-27 22:32:23 +0200

    Working version (MQTT and Led's)

--- ESP32_MQTT_SET/ESP32_MQTT_SET.ino
+++ ESP32_MQTT_SET/ESP32_MQTT_SET.ino
@@ -1,15 +1,14 @@
 #include "SET_Header.h"
 
-
 unsigned long timerUpdate;
-unsigned long ledSpeed = 200;
+unsigned long ledSpeed = 70;
 
 // WiFi credentials
 const char* ssid = "Pepper_C1-MUX-88B704";
 const char* password = "";
 
 // MQTT broker settings
-const char* mqtt_server = "192.168.100.2";
+const char* mqtt_server = "192.168.100.5";
 const int mqtt_port = 1883;
 const char* mqtt_user = "mqtt_user";
 const char* mqtt_password = "smartenergytable";
@@ -18,7 +17,7 @@ const char* mqtt_topic = "esp/test";
 WiFiClient espClient;
 PubSubClient client(espClient);
 
-const unsigned long timerDuration = 400;
+const unsigned long timerDuration = 1000;
 bool antennaFlag = false;
 int antenna;
 unsigned long antennaLastActive[LEDSECTIONS] = {0}; 
@@ -28,7 +27,7 @@ bool antennaActive[LEDSECTIONS] = {false};
 WipeEffect effects[LEDSECTIONS]; // Store animation state for each segment
 
 void __init__CD77_colorwipe(uint8_t y, CRGB color, uint32_t wait, bool reverse) {
-  effects[y].index = 0;
+  effects[y].index = reverse ? segments[y].size - 1 : 0;
   effects[y].active = false;
   effects[y].color = color;
   effects[y].lastUpdate = millis();
@@ -50,46 +49,43 @@ void callback(char* topic, byte* payload, unsigned int length) {
   int separatorIndex = message.indexOf(':');  
   String command = message.substring(0, separatorIndex);
   antenna = message.substring(separatorIndex + 1).toInt();
+  if (antenna <= 0 || antenna > 8) return;  // invalid antenna index
+  
+  // Determine how many segments are valid for this antenna
+  uint8_t segmentCount = (antenna == 4) ? 2 : 4;
+  
+  for (int i = 0; i < segmentCount; i++) {
+    uint8_t segmentIndex = antennaToSegments[antenna][i];
+    if (segmentIndex >= segments.size()) continue;
 
-    //antennaLastActive[antenna] = millis();
-    //antennaActive[antenna] = true;  // Mark it as active
-    /*
-    // Turn on LEDs for the corresponding section
-    if (antenna >= 0 && antenna < segments.size()) {
-      segments[antenna].effect.active = true;
-      antennaLastActive[antenna] = millis();
-      antennaActive[antenna] = true;
-      Serial.println("Segment activated.");
-    }
-    */
-    for (int i = 0; i < 5; i++) {
-        uint8_t segmentIndex = antennaToSegments[antenna][i];
+    // Only reset if animation is not running
+    if (!effects[segmentIndex].active) {
         effects[segmentIndex].active = true;
-        effects[segmentIndex].index = 0;  // Optionally reset effect position
-        antennaLastActive[segmentIndex] = millis();
-        antennaActive[segmentIndex] = true;
+        effects[segmentIndex].index = effects[segmentIndex].reverse ? segments[segmentIndex].size - 1 : 0;
     }
-    Serial.print("Antenna ");
-    Serial.print(antenna);
-    Serial.println(" activated.");
+
+    antennaLastActive[segmentIndex] = millis();
+    antennaActive[segmentIndex] = true;
+    Serial.printf("Activated segment %d for antenna %d\n", segmentIndex, antenna);
+  }
 }
 
 void setup_led(){
   // Define FastLED settings
-  FastLED.addLeds<CHIPSET, Data_PinS1>(ledsS1, 105);
-  FastLED.addLeds<CHIPSET, Data_PinS2>(ledsS2, 20);
-  FastLED.addLeds<CHIPSET, Data_PinS3>(ledsS3, 20);
-  FastLED.addLeds<CHIPSET, Data_PinS4>(ledsS4, 20);
-  FastLED.addLeds<CHIPSET, Data_PinS5>(ledsS5, 20);
-  FastLED.addLeds<CHIPSET, Data_PinS6>(ledsS6, 20);
-  FastLED.addLeds<CHIPSET, Data_PinS7>(ledsS7, 20);
+  FastLED.addLeds<CHIPSET, Data_PinS1>(ledsS1, 45);
+  FastLED.addLeds<CHIPSET, Data_PinS2>(ledsS2, 105);
+  FastLED.addLeds<CHIPSET, Data_PinS3>(ledsS3, 38);
+  FastLED.addLeds<CHIPSET, Data_PinS4>(ledsS4, 15);
+  FastLED.addLeds<CHIPSET, Data_PinS5>(ledsS5, 37);
+  FastLED.addLeds<CHIPSET, Data_PinS6>(ledsS6, 38);
+  FastLED.addLeds<CHIPSET, Data_PinS7>(ledsS7, 44);
   FastLED.setBrightness(BRIGHTNESS);
   FastLED.setMaxPowerInVoltsAndMilliamps(5, 1500);
   set_max_power_indicator_LED(13);
 
   // Initialise colorwipe function with WipeEffects atributes
-  for (int i = 0; i < LEDSECTIONS; i++){
-  __init__CD77_colorwipe(i, CRGB::Red, ledSpeed, 0);
+  for (int i = 0; i <= LEDSECTIONS; i++){
+    __init__CD77_colorwipe(i, segments[i].color, ledSpeed, segments[i].reverse);
   }
 }
 
@@ -117,117 +113,75 @@ void reconnect() {
         }
     }
 }
-
 // Non-blocking LED update function
 void updateCD77_colorwipe() {
   uint32_t now = millis();
+
   for (int i = 0; i < segments.size(); i++) {
+    auto& eff = effects[i];
     auto& seg = segments[i];
-    auto& eff = seg.effect;
 
     if (eff.active && now - eff.lastUpdate >= eff.interval) {
       eff.lastUpdate = now;
-      uint8_t idx = eff.index;
 
-      if ((eff.reverse && idx < seg.size - 3) || (!eff.reverse && idx > 2)) {
-        seg.leds[eff.reverse ? idx + 3 : idx - 3] = CRGB::Black;
-      }
-
-      if (idx < seg.size) {
-        seg.leds[idx] = eff.color;
-        if (eff.reverse) {
-          if (idx + 1 < seg.size) seg.leds[idx + 1] = eff.color - 100;
-          if (idx + 2 < seg.size) seg.leds[idx + 2] = eff.color - 200;
-        } else {
-          if (idx > 0) seg.leds[idx - 1] = eff.color - 100;
-          if (idx > 1) seg.leds[idx - 2] = eff.color - 200;
+      // Clear trailing pixels behind current index
+      for (int t = 1; t <= 3; ++t) {
+        int clearPos = eff.reverse ? eff.index + t : eff.index - t;
+        if (clearPos >= 0 && clearPos < seg.size) {
+          seg.leds[clearPos] = CRGB::Black;
         }
-        eff.index += eff.reverse ? -1 : 1;
+      }
+      // Draw head + trail
+      if (eff.index >= 0 && eff.index < seg.size) {
+        seg.leds[eff.index] = eff.color;
+
+        for (int t = 1; t <= 2; ++t) {
+          int trailPos = eff.reverse ? eff.index + t : eff.index - t;
+          if (trailPos >= 0 && trailPos < seg.size) {
+            seg.leds[trailPos] = eff.color;
+            seg.leds[trailPos].fadeToBlackBy(t * 100);
+          }
+        }
+      }
+      // Advance index
+      eff.index += eff.reverse ? -1 : 1;
+
+      if (eff.reverse) {
+        if (eff.index < -3) {
+          eff.index = seg.size - 1;
+        }
       } else {
-        eff.index = eff.reverse ? seg.size - 1 : 0;
+        if (eff.index > seg.size + 2) {
+          eff.index = 0;
+        }
       }
     }
-
+    // If antenna is not active > deactive leds
     if (antennaActive[i] && (now - antennaLastActive[i] >= timerDuration)) {
       eff.active = false;
       fill_solid(seg.leds, seg.size, CRGB::Black);
       antennaActive[i] = false;
-    }
+    } 
   }
   FastLED.show();
 }
- /* 
-  uint32_t now = millis();
 
-  for (uint8_t y = 0; y < LEDSECTIONS; y++) {
-    if (effects[y].active && now - effects[y].lastUpdate >= effects[y].interval) {
-      effects[y].lastUpdate = now;
-
-      uint8_t i = effects[y].index;
-
-      // Clear previous position
-      if ((effects[y].reverse && i < sizearray[y] - 3) || (!effects[y].reverse && i > 2)) {
-        ledarray[y][effects[y].reverse ? i + 3 : i - 3] = CRGB::Black;
-      }
-
-      // Set the new moving "wipe" with fading effect
-      if (i >= 0 && i < sizearray[y]) {
-        ledarray[y][i] = effects[y].color;
-        if (effects[y].reverse) {
-          if (i < sizearray[y] - 1) ledarray[y][i + 1] = effects[y].color - 100;
-          if (i < sizearray[y] - 2) ledarray[y][i + 2] = effects[y].color - 200;
-        } else {
-          if (i > 0) ledarray[y][i - 1] = effects[y].color - 100;
-          if (i > 1) ledarray[y][i - 2] = effects[y].color - 200;
-        }
-
-        // Move index in correct direction
-        effects[y].index += effects[y].reverse ? -1 : 1;
-      } else {
-        // Reset index to loop the animation and 
-        if (effects[y].reverse) {
-          if (i < sizearray[y] - 1) ledarray[y][i + 1] = CRGB::Black;
-          if (i < sizearray[y] - 2) ledarray[y][i + 2] = CRGB::Black;
-        } else {
-          if (i > 0) ledarray[y][i - 1] = CRGB::Black;
-          if (i > 1) ledarray[y][i - 2] = CRGB::Black;
-        }
-        effects[y].index = effects[y].reverse ? sizearray[y] - 1 : 0;
-      }
-      
-    }
-  }
-
-  for (uint8_t y = 1; y < LEDSECTIONS; y++) {
-    if (antennaActive[y] && (now - antennaLastActive[y] >= timerDuration)) {
-      Serial.print("Antenna ");
-      Serial.print(y);
-      Serial.println(" has been inactive, turning off LEDs.");
-
-      for (int i = 0; i <= y; i++){
-        effects[i].active = false;
-      }
-      effects[y].active = false;  // Disable this antenna's LED effect
-      fill_solid(ledarray[y], sizearray[y], CRGB::Black);  // Turn off only this section
-      antennaActive[y] = false;  // Mark it as inactive
-      }
-    }
-  FastLED.show();
-}
-*/
 void setup() {
     Serial.begin(115200);
     setup_wifi();
     setup_led();
-    effects[0].active = true;
+    FastLED.show();
     client.setServer(mqtt_server, mqtt_port);
     client.setCallback(callback);
 }
 
 void loop() {
+    
     if (!client.connected()) {
         reconnect();
     }
     client.loop();
     updateCD77_colorwipe();
+    //delay(10); // Smooth timing
+    FastLED.show();
 }
--- ESP32_MQTT_SET/SET_Header.h
+++ ESP32_MQTT_SET/SET_Header.h
@@ -20,11 +20,11 @@
 
 #define CHIPSET WS2812B
 #define BRIGHTNESS 50
-#define LEDSECTIONS 15
+#define LEDSECTIONS 31
 
 
 struct WipeEffect {
-    uint8_t index = 0;
+    int index = 0;
     bool active = false;
     bool reverse = false;
     CRGB color;
@@ -35,42 +35,66 @@ struct WipeEffect {
 struct LEDSegment {
     CRGBSet leds;
     uint8_t size;
-    WipeEffect effect;
+    bool reverse;
+    CRGB color;
+    //WipeEffect effect;
 
-    LEDSegment(CRGBSet set, uint8_t s) : leds(set), size(s) {}
+    LEDSegment(CRGBSet set, uint8_t s, bool rev, CRGB clr) : leds(set), size(s), reverse(rev), color(clr){}
 };
 
-CRGB ledsS1[105];
-CRGB ledsS2[20], ledsS3[20], ledsS4[20], ledsS5[20], ledsS6[20], ledsS7[20];
+CRGB ledsS1[45];
+CRGB ledsS2[105], ledsS3[38], ledsS4[15], ledsS5[37], ledsS6[38], ledsS7[44];
 
 std::vector<LEDSegment> segments = {
-    LEDSegment(CRGBSet(ledsS1, 0, 11), 12),
-    LEDSegment(CRGBSet(ledsS1, 12, 26), 15),
-    LEDSegment(CRGBSet(ledsS1, 29, 32), 4),
-    LEDSegment(CRGBSet(ledsS1, 33, 36), 4),
-    LEDSegment(CRGBSet(ledsS1, 41, 55), 15),
-    LEDSegment(CRGBSet(ledsS1, 63, 77), 15),
-    LEDSegment(CRGBSet(ledsS1, 79, 82), 4),
-    LEDSegment(CRGBSet(ledsS1, 83, 86), 4),
-    LEDSegment(CRGBSet(ledsS1, 90, 104), 15),
-    LEDSegment(CRGBSet(ledsS2, 0, 19), 20),
-    LEDSegment(CRGBSet(ledsS3, 0, 19), 20),
-    LEDSegment(CRGBSet(ledsS4, 0, 19), 20),
-    LEDSegment(CRGBSet(ledsS5, 0, 19), 20),
-    LEDSegment(CRGBSet(ledsS6, 0, 19), 20),
-    LEDSegment(CRGBSet(ledsS7, 0, 19), 20)
+    LEDSegment(CRGBSet(ledsS2, 0, 11), 12, true, CRGB::Red), // Middle
+    LEDSegment(CRGBSet(ledsS2, 12, 26), 14, false, CRGB::Red),// Segment 1
+    LEDSegment(CRGBSet(ledsS2, 29, 32), 3, false, CRGB::Blue),
+    LEDSegment(CRGBSet(ledsS2, 33, 36), 3, false, CRGB::Green),
+    LEDSegment(CRGBSet(ledsS2, 41, 55), 14, true, CRGB::Purple),
+    LEDSegment(CRGBSet(ledsS2, 63, 77), 15, false, CRGB::Red), // Segment 2 (Chained)
+    LEDSegment(CRGBSet(ledsS2, 79, 82), 3, false, CRGB::Blue),
+    LEDSegment(CRGBSet(ledsS2, 83, 86), 3, false, CRGB::Green),
+    LEDSegment(CRGBSet(ledsS2, 90, 104), 14, true, CRGB::Purple),
+    
+    LEDSegment(CRGBSet(ledsS3, 2, 9), 8, false, CRGB::Purple), // Segment 3
+    LEDSegment(CRGBSet(ledsS3, 14, 18), 4, false, CRGB::Blue),
+    LEDSegment(CRGBSet(ledsS3, 19, 23), 4, false, CRGB::Green),
+    LEDSegment(CRGBSet(ledsS3, 27, 38), 11, true, CRGB::Red),
+
+    LEDSegment(CRGBSet(ledsS5, 3, 8), 6, false, CRGB::Purple), // Segment 4
+    LEDSegment(CRGBSet(ledsS5, 13, 17), 5, false, CRGB::Blue),
+    LEDSegment(CRGBSet(ledsS5, 19, 24), 5, false, CRGB::Green),
+    LEDSegment(CRGBSet(ledsS5, 28, 37), 9, true, CRGB::Red),
+
+    LEDSegment(CRGBSet(ledsS6, 3, 11), 7, false, CRGB::Purple), // Segment 5
+    LEDSegment(CRGBSet(ledsS6, 14, 18), 4, false, CRGB::Blue),
+    LEDSegment(CRGBSet(ledsS6, 19, 23), 4, false, CRGB::Green),
+    LEDSegment(CRGBSet(ledsS6, 26, 38), 11, true, CRGB::Red),
+
+    LEDSegment(CRGBSet(ledsS7, 4, 17), 13, false, CRGB::Purple),  // Segment 6
+    LEDSegment(CRGBSet(ledsS7, 20, 23), 3, false, CRGB::Blue),
+    LEDSegment(CRGBSet(ledsS7, 24, 27), 3, false, CRGB::Green),
+    LEDSegment(CRGBSet(ledsS7, 30, 44), 14, true, CRGB::Red),
+
+    LEDSegment(CRGBSet(ledsS1, 3, 14), 12, false, CRGB::Purple), // Segment 7
+    LEDSegment(CRGBSet(ledsS1, 19, 22), 3, false, CRGB::Blue),
+    LEDSegment(CRGBSet(ledsS1, 23, 26), 3, false, CRGB::Green),
+    LEDSegment(CRGBSet(ledsS1, 30, 45), 15, true, CRGB::Red),
+
+    LEDSegment(CRGBSet(ledsS4, 7, 15), 9, true, CRGB::Red) // Segment 8    
 };
 
 // For 8 antennas (0 to 7), define segment indexes controlled by each
-const uint8_t antennaToSegments[8][4] = {
-    {1, 2, 3, 4},       // Antenna 0: 4 segments
-    {5, 6, 7, 8},       // Antenna 1: 4 segments
-    {9, 10, 11, 12},    // Antenna 2: 4 segments
-    {13, 14, 15, 16},   // Antenna 3: 4 segments
-    {17, 18, 19, 20},   // Antenna 4: 4 segments
-    {21, 22, 23, 24},   // Antenna 5: 4 segments
-    {25, 26, 27, 28},   // Antenna 6: 4 segments
-    {29}                // Antenna 7: 1 segment
+const uint8_t antennaToSegments[9][4] = {
+    {0, 0, 0, 0},       // Antenna 1: 4 segments
+    {9, 10, 11, 12},        // Antenna 2: 4 segments
+    {5, 6, 7, 8},           // Antenna 3: 4 segments
+    {1, 2, 3, 4},           // Antenna 4: 4 segments
+    {0, 29},                
+    {25, 26, 27, 28},       // Antenna 5: 2 segments
+    {21, 22, 23, 24},       // Antenna 6: 4 segments
+    {17, 18, 19, 20},       // Antenna 7: 4 segments
+    {13, 14, 15, 16}        // Antenna 8: 4 segments
 };
 
 #endif //SET_HEADER_H
